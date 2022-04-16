2ba5d3c4bbcffb871f9b9f43591418d719cbbc5a
youone04 <yudi.gunaone87@gmail.com>
2022-04-16 09:51:21 +0700

    udpate origin

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const {router} = require('./routes');
 require('dotenv').config();
 const cors = require('cors');
 const corsOptions ={
-    origin: '*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 200
 }
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
